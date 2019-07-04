f5a8717451a9698a4484b6a039a0a9ad6a18ba2e
Kori SEFEANE <ksefeane@c4r12s9.wethinkcode.co.za>
2019-07-04 17:53:39 +0200

    attempt

--- get_next_line.c
+++ get_next_line.c
@@ -5,21 +5,23 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ksefeane <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/07/01 10:18:23 by ksefeane          #+#    #+#             */
-/*   Updated: 2019/07/04 17:21:53 by ksefeane         ###   ########.fr       */
+/*   Created: 2019/07/04 17:47:34 by ksefeane          #+#    #+#             */
+/*   Updated: 2019/07/04 17:53:19 by ksefeane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static int	
+static int	read_cache(
 
-int		get_next_line(const int fd, char **line)
+int			get_next_line(const int fd, char **line)
 {
-	char		*w;
 	static char	*cache[1024];
-	int			r;
-	int			e;
+	char		*w;
 
 	w = ft_strnew(BUFF_SIZE);
+	if (fd < 0 || read(fd, w, 0) < 0 || !line)
+		return (-1);
+	if (!cache[fd] && !(cache[fd] = ft_strnew(0)))
+		return (-1);
 }
